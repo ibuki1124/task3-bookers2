90e2eabed4b73354a55f8575ed14123b08e44621
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2024-06-18 11:43:23 +0000

    [Add] ユーザ情報(User info)の表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,11 +2,13 @@ class BooksController < ApplicationController
   def show
     @book_new = Book.new
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def index
     @book = Book.new
     @books = Book.all
+    @user = current_user
   end
   
   def create
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
 
   def index
+    @user = current_user
   end
 
   def edit
