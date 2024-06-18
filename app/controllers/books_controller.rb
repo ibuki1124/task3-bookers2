6cc83f2b4ab0791030a2464fd9f8c5e6aa2512b0
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2024-06-18 13:58:02 +0000

    [Add] ユーザ情報と投稿内容のバリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,30 +10,38 @@ class BooksController < ApplicationController
     @books = Book.all
     @user = current_user
   end
-  
+
   def create
-    book = Book.new(book_params)
-    book.user_id = current_user.id
-    book.save
-    redirect_to book_path(book)
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
   end
 
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
