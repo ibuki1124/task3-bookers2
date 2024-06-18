c1b8c8ce2bae61757767b7d590eb63a4b96fcb5b
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2024-06-18 12:15:10 +0000

    [Add] ユーザの一覧・詳細表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def index
     @user = current_user
+    @users = User.all
   end
 
   def edit
